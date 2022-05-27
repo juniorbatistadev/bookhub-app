7648de81770b5c7f551c64f8e76c77cb9d74436b
Junior Batista <31187922+juniorbatistaRD@users.noreply.github.com>
2022-05-27 13:04:02 -0400

    2nd try

--- App.js
+++ App.js
@@ -1,10 +1,38 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+} from "@react-native-google-signin/google-signin";
 
 export default function App() {
+  const handleLogin = async () => {
+    GoogleSignin.configure({
+      webClientId:
+        "950764784194-mt7u0qevqkfv232s7cd6vqqm22h14tag.apps.googleusercontent.com",
+    });
+
+    const { idToken } = await GoogleSignin.signIn();
+
+    console.log(idToken);
+
+    // Create a Google credential with the token
+    const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+
+    // Sign-in the user with the credential
+    return auth().signInWithCredential(googleCredential);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start!!! working on your app!</Text>
+      {/* <Text>Open up App.js to start!!! working on your app!</Text> */}
+      <GoogleSigninButton
+        style={{ width: 192, height: 48 }}
+        size={GoogleSigninButton.Size.Wide}
+        color={GoogleSigninButton.Color.Dark}
+        onPress={handleLogin}
+      />
+
       <StatusBar style="auto" />
     </View>
   );
